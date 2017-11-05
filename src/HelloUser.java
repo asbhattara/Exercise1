f975937b7d049afc5f81d087614e81f760b9337d
asbhattara <as.bhattarai@jacobs-university.de>
2017-11-05 22:10:32 +0100

    Added HelloUser class and updated helloworld

---
+++ src/HelloUser.java
@@ -0,0 +1,16 @@
+
+public class HelloUser {
+	
+	private String userName;
+	
+	public HelloUser(String user) {
+		userName = user;
+	}
+
+	public void greetUser() {
+		
+		System.out.println("Hello "+userName+"!");
+	}
+	
+	
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		
+		
 		System.out.println("Hello ASE2017 - How are you? :)");
+		
+		System.out.print("Please enter your name: ");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		HelloUser newUser = new HelloUser(sc.next());
+		
+		newUser.greetUser();
+		
+		
 	}
 
 }
